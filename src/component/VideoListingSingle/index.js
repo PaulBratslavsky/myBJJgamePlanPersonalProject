370535fcbf4b9ffd7c0e780fa8c870d57a5f0bf1
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2019-03-02 12:38:17 -0500

    Set up basic form that takes the data and changes it to a json format file.  Next step is to create a post http request that creates a new post.

--- src/component/VideoListingSingle/index.js
+++ src/component/VideoListingSingle/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import ReactHtmlParser from 'react-html-parser';
 import { Link } from "react-router-dom";
 
+import { processVideoUrl } from '../ProcessVideoUrl';
+
 export default class VideoListSingle extends Component {
   render() {
 
@@ -20,14 +22,26 @@ export default class VideoListSingle extends Component {
     const postOptions = ReactHtmlParser(acf.options);
     const postTags = ReactHtmlParser(acf.tags);
     const postExcerpt = ReactHtmlParser(excerpt.rendered);
-
+    const postVideoUrl = acf.video_url;
+    
+    console.log(postVideoUrl, "Should be video url to use for post");
     return (
+        
         <div className="video">
           <header className="video-header">
             <h2 className="text-uppercase">{postTitle}</h2>
           </header>
 
-          <div className="videoWrapper">{postVideo}</div> 
+          <div className="videoWrapper">
+            <iframe 
+              width="560" 
+              height="315" 
+              src={processVideoUrl(postVideoUrl)}
+              frameBorder="0" 
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+              allowFullScreen>
+            </iframe>
+          </div>
           
           <div className="content">
             <div className="tags-container">
