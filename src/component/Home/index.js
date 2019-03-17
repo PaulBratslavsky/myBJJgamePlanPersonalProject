997fbb8512f05283b1f60d609d24184c65cbbce5
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2019-03-17 12:38:33 -0400

    current working build.

--- src/component/Home/index.js
+++ src/component/Home/index.js
@@ -8,8 +8,8 @@ export default class Home extends Component {
       <div style={{height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
         <Link style={{marginBottom: '15px'}} className="btn btn-primary" to={'/videolist'}>Log In</Link>
         <Link className="btn btn-success" to={'/letstalk'}>Let's Talk</Link>
-        <small style={{color: "white"}}>This is a test feature that I am working on now.</small>
-        <Link className="btn btn-success" to={'/addvideopost'}>Add Video</Link>
+        { /* <small style={{color: "white"}}>This is a test feature that I am working on now.</small> */ }
+        { /* <Link className="btn btn-success" to={'/addvideopost'}>Add Video</Link> */ } 
       </div>
     )
   }
